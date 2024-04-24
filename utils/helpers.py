48fc725c1d74ee41e764bbfa38244a495b41e20d
Arbel Amram <arbelamram@gmail.com>
2024-04-24 18:01:18 +0300

    reorder functions

--- utils/helpers.py
+++ utils/helpers.py
@@ -5,17 +5,14 @@ from entities import Tile
 from assets import *
 
 
-def get_random_pos(tiles):
-    row = None
-    col = None
-    while True:
-        row = random.randrange(0, ROWS)
-        col = random.randrange(0, COLS)
+def draw(window, tiles):
+    window.fill(BACKGROUND_COLOR)
 
-        if f"{row}{col}" not in tiles:
-            break
+    for tile in tiles.values():
+        tile.draw(window)
 
-    return row, col
+    draw_grid(window)
+    pygame.display.update()
 
 
 def draw_grid(window):
@@ -30,29 +27,32 @@ def draw_grid(window):
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
 
+def get_random_pos(tiles):
+    row, col = None, None
+    
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+
+        if f"{row}{col}" not in tiles:
+            break
+
+    return row, col
+
+
+def generate_tiles():
+    tiles = {}
+    
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+
+    return tiles
+
+
 def update_tiles(window, tiles, sorted_tiles):
     tiles.clear()
     for tile in sorted_tiles:
         tiles[f"{tile.row}{tile.col}"] = tile
 
     draw(window, tiles)
-
-
-def draw(window, tiles):
-    window.fill(BACKGROUND_COLOR)
-
-    for tile in tiles.values():
-        tile.draw(window)
-
-    draw_grid(window)
-
-    pygame.display.update()
-
-
-def generate_tiles():
-    tiles = {}
-    for _ in range(2):
-        row, col = get_random_pos(tiles)
-        tiles[f"{row}{col}"] = Tile(2, row, col)
-
-    return tiles
