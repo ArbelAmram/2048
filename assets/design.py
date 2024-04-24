55f38a09125e02efb18229a1890ef7259cfb4674
Arbel Amram <arbelamram@gmail.com>
2024-04-24 17:50:18 +0300

    remove comments

--- assets/design.py
+++ assets/design.py
@@ -1,21 +1,16 @@
-import pygame
-
 OUTLINE_THICKNESS = 10
 
-# Color defined by RGB
 OUTLINE_COLOR = (187, 173, 160)
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110 ,101)
 
-WIDTH, HEIGHT = 800, 800 # ROI
-ROWS = 4
-COLS = 4
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 4, 4
 
-RECT_HEIGHT = HEIGHT // ROWS # integer devision
-RECT_WIDTH = WIDTH // COLS # integer devision
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
 
-FPS = 60 # frames per second
-
-MOVE_VEL = 20 # the speed at which the tiles will move - 20 pixels per second
+FPS = 60
+MOVE_VEL = 20
 
 
