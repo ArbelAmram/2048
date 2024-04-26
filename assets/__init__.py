2ae6b63df75e4e0fd6c099e47dfc4c042f712c72
Arbel Amram <arbelamram@gmail.com>
2024-04-26 13:23:34 +0300

    remove comments

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,6 +1,3 @@
-# This file is referring 'assets' folder as a python package
-# When a file from 'assets' is imported __init__.py file will run first
-
 from .design import *
 
 print("__init__.py ran from assets")
--- entities/__init__.py
+++ entities/__init__.py
@@ -1,6 +1,3 @@
-# This file is referring 'entities' folder as a python package
-# When any file from 'entities' is imported __init__.py file will run first
-
 from .tile import Tile
 
 print("__init__.py ran from entities")
