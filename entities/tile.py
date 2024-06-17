3f7840d8d01a283b89205a3d40cf7b83bbf8bb1c
Arbel Amram <arbelamram@gmail.com>
2024-06-17 13:35:44 +0300

    remove comments

--- entities/tile.py
+++ entities/tile.py
@@ -10,15 +10,7 @@ DEFAULT_FONT_BOLD = True
 FONT = pygame.font.SysFont(DEFAULT_FONT_NAME, DEFAULT_FONT_SIZE, bold=DEFAULT_FONT_BOLD)
 
 class Tile:
-    '''
-    A class to represent a single tile in the 2048 game.
-    '''
     def __init__(self, value, row, col, font=FONT, font_color=FONT_COLOR):
-        '''
-        Initialize the tile with a value, position, and optional font settings.
-        '''
-        
-        # Error Handling
         if value <= 0 or (value & (value - 1)) != 0:
             raise ValueError("Tile value must be a power of 2 greater than 0.")
         
@@ -29,12 +21,9 @@ class Tile:
         self.y = row * RECT_HEIGHT
         self.font = font
         self.font_color = font_color
-        self.color = self._get_color() # Optimization - Cached the tile's color in self.color to avoid recalculating it multiple times
+        self.color = self._get_color()
 
     def _get_color(self):
-        '''
-        Determine the color of the tile based on its value.
-        '''
         color_index = int(math.log2(self.value)) - 1
         if color_index < len(COLORS):
             return COLORS[color_index]
@@ -43,9 +32,6 @@ class Tile:
             return (255, 255, 255)
         
     def draw(self, window):
-        '''
-        Draw the tile on the given window.
-        '''
         pygame.draw.rect(window, self.color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
 
         text = self.font.render(str(self.value), True, self.font_color)
@@ -58,9 +44,6 @@ class Tile:
         )
 
     def set_pos(self, ceil=False):
-        '''
-        Set the tile's row and column based on its pixel position.
-        '''
         if ceil:
             self.row = math.ceil(self.y / RECT_HEIGHT)
             self.col = math.ceil(self.x / RECT_WIDTH)
@@ -69,19 +52,11 @@ class Tile:
             self.col = math.floor(self.x / RECT_WIDTH)
 
     def move(self, delta):
-        '''
-        Move the tile by the specified delta.
-        '''
         self.x += delta[0]
         self.y += delta[1]
         self.set_pos()
 
     def update_value(self, new_value):
-        '''
-        Update the tile's value and recalculate its color.
-        Error Handling - change the tile's value and update its color
-        '''
-
         if new_value <= 0 or (new_value & (new_value - 1)) != 0:
             raise ValueError("Tile value must be a power of 2 greater than 0.")
         
